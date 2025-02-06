aa29701c03ec041570cef9661d6b1a6f81ac9661
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2025-02-06 21:18:45 +0100

    Adding latest updates in terraform

--- containers/app/webbot.py
+++ containers/app/webbot.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import FirefoxOptions
-# from selenium.webdriver import ChromeOptions
 from bs4 import BeautifulSoup
 
 
--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,4 +1,4 @@
-# resource "aws_cloudwatch_log_group" "ecs_log_group" {
-#   name              = "/ecs/my-app-service"
-#   retention_in_days = 7
-# }
+resource "aws_cloudwatch_log_group" "ecs_log_group" {
+  name              = "/ecs/my-app-service"
+  retention_in_days = 7
+}
